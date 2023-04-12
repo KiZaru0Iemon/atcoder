c8a4eb66fc26db74b56ff55dbb8266c1855ebab5
KiZaru0Iemon <kizaru0130@me.com>
2023-04-12 18:27:29 +0900

    2023/4/13 grid途中

--- simu/grid_abc88.cpp
+++ simu/grid_abc88.cpp
@@ -4,8 +4,57 @@ using namespace std;
 using ll=long long int;
 using P=pair<int,int>;
 
+int H,W;
+vector<vector<char>> s;
+vector<vector<char>> visited;
+vector<vector<int>> dist;
+int dire[][2]={{0,1},{0,-1},{1,0},{-1,0}};
+
+void bfs()
+{
+  queue<P> q;
+  q.push({0,0});
+  visited[0][0]=false;
+  dist[0][0]=0;
+
+  while(q.size()){
+    P c=q.front(); q.pop();
+    int cf=c.first;
+    int cs=c.second;
+    rep(i,4){
+      int uf=cf+dire[i][0];
+      int us=cs+dire[i][1];
+      P u={uf,us};
+      if(uf<0 || uf>=H || us<0 || us>=W )continue;
+      if()continue;
+    }
+  }
+}
+
 int main()
 {
+  int bnum=0;
+
+  //入力とメモリ確保
+  cin>>H>>W;
+  s.resize(H);
+  visited.resize(H);
+  dist.resize(H);
+  rep(i,H){
+    s[i].resize(W);
+    visited[i].assign(W,false);
+    dist[i].assign(W,0);
+  }
+  rep(i,H){
+    rep(j,W){
+      cin>>s[i][j];
+      if(s[i][j]=='#')
+        bnum++;
+    }
+  }
+
+  bfs();
+  cout<<(H*W-bnum-dist[H-1][W-1]-1)<<endl;
 
   return 0;
 }
